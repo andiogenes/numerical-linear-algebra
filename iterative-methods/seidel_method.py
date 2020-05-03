d12cc820ec5b27bc9490bd0b266b3f45f078799e
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-05-03 23:30:05 +0700

    Write primary code for Seidel Method

---
+++ iterative-methods/seidel_method.py
@@ -0,0 +1,22 @@
+def solve_by_seidel(matrix, terms, eps):
+    end = len(matrix)
+
+    iterations_count = 0
+    x = [0.0] * end
+
+    while True:
+        x_k = [v for v in x]
+        for i in range(0, end):
+            s1 = sum(matrix[i][j] * x_k[j] for j in range(0, i))
+            s2 = sum(matrix[i][j] * x[j] for j in range(i + 1, end))
+            x_k[i] = (terms[i] - s1 - s2) / matrix[i][i]
+
+        norm = max([(abs(x[i] - v)) for i, v in enumerate(x_k)])
+        x = x_k
+
+        iterations_count += 1
+
+        if norm <= eps:
+            break
+
+    return x, iterations_count

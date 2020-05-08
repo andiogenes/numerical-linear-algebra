fe49e8fe98cc6f370dff41c83326b0f06976f901
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-05-08 22:52:47 +0700

    Create program entry

--- square-root-method/main.py
+++ square-root-method/main.py
@@ -1,14 +1,39 @@
-from square_root_method import decompose, solve
+import argparse
 
-matrix = [
-    [1.0, 0.0, 0.0],
-    [0.0, 1.0, 0.0],
-    [0.0, 0.0, 1.0]
-]
+import yaml
 
-terms = [1.0, 1.0, 1.0]
 
-a, b = decompose(matrix)
+def process_square_root(_args):
+    # Загрузка входных данных из YAML-файла
+    with open(_args.source, 'r') as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as e:
+            print(e)
+            return
 
-print(a, b)
-print(solve(matrix, terms))
+    # Сохранение результатов в файл
+    with open(_args.dest, 'w') as stream:
+        try:
+            yaml.dump(report, stream)
+        except yaml.YAMLError as e:
+            print(e)
+            return
+
+
+def parse_command_line():
+    __parser = argparse.ArgumentParser(description='')
+    __parser.add_argument('--source', dest='source', type=str, default='assignment.yml')
+    __parser.add_argument('--dest', dest='dest', type=str, default='result.yml')
+    __parser.set_defaults(func=process_square_root)
+
+    return __parser
+
+
+if __name__ == "__main__":
+    _parser = parse_command_line()
+    args = _parser.parse_args()
+    try:
+        args.func(args)
+    except AttributeError:
+        _parser.parse_args(['--help'])
